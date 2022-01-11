225582234b6769ed4d9073d29d004f82eae0b2a5
berendjan10 <43063044+berendjan10@users.noreply.github.com>
2022-01-11 20:20:35 +0100

    310x gerund

--- RRT_p_carsim.py
+++ RRT_p_carsim.py
@@ -156,7 +156,7 @@ def RRT_p_simulation(rectangle_inflation):
 
 
 if __name__ == '__main__':
-    num_sims = 100 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    num_sims = 310 # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
     simulation_number_list = []
     rectangle_inflation_param_list = []
@@ -166,7 +166,8 @@ if __name__ == '__main__':
     planner_successful_list = []
     out_of_time_list = []
     for i in range(num_sims):
-        rectangle_inflation = 10*(i % 7) # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+        print(i)
+        rectangle_inflation = 2*(i % 31) # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
         num_iter = None
         pathplanner_successful = None
         RRT_solution_time = None
@@ -198,7 +199,7 @@ if __name__ == '__main__':
         }
     
     )
-    save_to_pickle = False    #<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    save_to_pickle = True    #<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     if save_to_pickle:
         df.to_pickle("rrt_+_simulation_results.pkl")
     
